e8dc95aaa1ba9dca149e77c046455db2245c9964
gianlucaparadise <gianlucaparadise@gmail.com>
2019-11-07 07:35:48 +0100

    Very Basic chromecast setup

---
+++ android/src/main/kotlin/com/gianlucaparadise/flutter_cast_framework/cast/CastOptionsProvider.kt
@@ -0,0 +1,19 @@
+package com.gianlucaparadise.flutter_cast_framework.cast
+
+import android.content.Context
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+
+
+class CastOptionsProvider : OptionsProvider {
+    override fun getCastOptions(context: Context): CastOptions {
+        return CastOptions.Builder()
+                .setReceiverApplicationId("4F8B3483")
+                .build()
+    }
+
+    override fun getAdditionalSessionProviders(context: Context): List<SessionProvider>? {
+        return null
+    }
+}
